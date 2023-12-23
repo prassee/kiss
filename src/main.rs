f479c2b22975222675be0b2044744a0ebfd36543
Prasanna <Prasanna>
2023-12-23 17:28:27 +0530

    const for output path

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use std::{
     fs::{File, OpenOptions},
     io::{self, BufRead, Write},
+    str,
 };
 
 use serde_json::Value;
+
+const KITTY_PATH: &str = "/tmp/kitty-sesison.kitty";
 
 fn main() {
     let input = io::stdin()
@@ -17,10 +20,10 @@ fn main() {
 fn parse_kitty_session(data: &str) {
     let values: Value = serde_json::from_str(data).unwrap();
     // create a file to stage the chagnes
-    File::create("/tmp/kiss.txt").expect("file not found");
+    File::create(KITTY_PATH).expect("file not found");
     values[0]["tabs"].as_array().unwrap().iter().for_each(|x| {
         let tab_config = format!(
-            "\n new_tab {:} \nlayout {:} \ncd {:} \ntitle {:} \nlaunch --env KITTY_WINDOW_ID={:} --env PWD=/home/saipranav \nfocus\n",
+            "\nnew_tab {:} \nlayout {:} \ncd {:} \ntitle {:} \nlaunch --env KITTY_WINDOW_ID={:} --env PWD=/home/saipranav \nfocus\n",
             x["title"].as_str().unwrap(), x["layout"].as_str().unwrap(), x["windows"][0]["cwd"].as_str().unwrap(), x["windows"][0]["title"].as_str().unwrap(),x["windows"][0]["id"].as_number().unwrap().to_string()
         );
         append_to_file(tab_config);
@@ -34,10 +37,9 @@ fn parse_kitty_session(data: &str) {
 fn append_to_file(tab_config: String) {
     let mut data_file = OpenOptions::new()
         .append(true)
-        .open("/tmp/kiss.txt")
+        .open(KITTY_PATH)
         .expect("output file doesnot exist");
     data_file
         .write(tab_config.as_bytes())
         .expect("write failed");
-    println!("{:}", tab_config);
 }
